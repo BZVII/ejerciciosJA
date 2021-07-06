bb1bce55d039cb5d83a162becf1639a2272f771b
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-07-06 10:57:59 +0200

    Primer ejercicio

---
+++ ejercicios/ejercicio1.py
@@ -0,0 +1,19 @@
+'''
+Debes obtener estos tres valores
+   - Total de euros que se ha gastado el usuario: Es la suma de todas las cantidades_from cuando moneda_from es EUR
+   - Total de euros que ha recuperado el usuario: Es la suma de todas las cantidades_to cuando moneda_to es EUR
+   - Dinero aún atrapado en la inversión: Recuperado - Gastado
+
+Tienes que hacer un programa python que acceda a la base de datos que está en este proyecto data/movimientos.db
+Y te tiene que imprimir lo siguiente
+
++------------------------+
+| Gastado...:  8305.00 € |
+| Recuperado: 17512.49 € |
++------------------------+
+| Atrapado..:  9207.49 € |
++------------------------+
+
+'''
+
+
